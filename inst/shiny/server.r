ed9715eba41774d727169c54796a9bcbcc997a4f
Boyan Angelov <boyan.angelov@gmail.com>
2018-09-22 17:20:28 +0200

    Add explicit package referral for Shiny app - support for GalaxyE integration

--- inst/shiny/server.r
+++ inst/shiny/server.r
@@ -19,7 +19,7 @@ server <- function(input, output) {
             detail = "Downloading species occurence data and climate
             variables...")
 
-        benchmarking_data <- get_benchmarking_data(
+        benchmarking_data <- sdmbench::get_benchmarking_data(
             scientific_name = input$text,
             limit = input$limit,
             climate_type = input$climate_type,
@@ -30,7 +30,7 @@ server <- function(input, output) {
 
         progress$inc(1/4, detail = "Processing data...")
 
-        benchmarking_data$df_data <- partition_data(
+        benchmarking_data$df_data <- sdmbench::partition_data(
             dataset_raster = benchmarking_data$raster_data,
             dataset = benchmarking_data$df_data,
             env = benchmarking_data$raster_data$climate_variables,
@@ -77,83 +77,83 @@ server <- function(input, output) {
         }
 
         benchmarking_data$df_data <- na.omit(benchmarking_data$df_data)
-        bmr <- benchmark_sdm(benchmarking_data = benchmarking_data$df_data,
+        bmr <- sdmbench::benchmark_sdm(benchmarking_data = benchmarking_data$df_data,
                              learners = learners,
                              dataset_type =  partitioning_type,
                              sample = input$sample)
 
         progress_bmr$inc(1/3, detail = "Plotting benchmark results...")
         output$bmr_plot1 <- renderPlot(plotBMRBoxplots(bmr, measure = mlr::auc))
-        output$bmr_results <- renderTable(get_best_model_results(bmr))
+        output$bmr_results <- renderTable(sdmbench::get_best_model_results(bmr))
 
         bmr_models <- mlr::getBMRModels(bmr)
-        best_results <- get_best_model_results(bmr)
+        best_results <- sdmbench::get_best_model_results(bmr)
 
         output$model_map_1 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[1],
                         model_iteration = best_results$iter[1],
                         map_type = "interactive"))
 
         output$model_map_2 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[2],
                         model_iteration = best_results$iter[2],
                         map_type = "interactive"))
 
         output$model_map_3 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[3],
                         model_iteration = best_results$iter[3],
                         map_type = "interactive"))
 
         output$model_map_4 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[4],
                         model_iteration = best_results$iter[4],
                         map_type = "interactive"))
 
         output$model_map_5 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[5],
                         model_iteration = best_results$iter[5],
                         map_type = "interactive"))
 
         output$model_map_6 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[6],
                         model_iteration = best_results$iter[6],
                         map_type = "interactive"))
 
         output$model_map_7 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[7],
                         model_iteration = best_results$iter[7],
                         map_type = "interactive"))
 
         output$model_map_8 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[8],
                         model_iteration = best_results$iter[8],
                         map_type = "interactive"))
 
         output$model_map_9 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[9],
                         model_iteration = best_results$iter[9],
                         map_type = "interactive"))
 
         output$model_map_10 <- leaflet::renderLeaflet(
-            plot_sdm_map(raster_data = benchmarking_data$raster_data,
+            sdmbench::plot_sdm_map(raster_data = benchmarking_data$raster_data,
                         bmr_models = bmr_models,
                         model_id = best_results$learner.id[10],
                         model_iteration = best_results$iter[10],
@@ -174,7 +174,7 @@ server <- function(input, output) {
             progress_meeval$set(message = "Status", value = 0)
             progress_meeval$inc(1/3, detail = "Evaluating MaxEnt...")
 
-            maxent_results <- evaluate_maxent(raster_data = v$data$raster_data,
+            maxent_results <- sdmbench::evaluate_maxent(raster_data = v$data$raster_data,
                                               method = v$partitioning_type)
 
             progress_meeval$inc(1/3, detail = "Plotting MaxEnt map")
@@ -217,12 +217,12 @@ server <- function(input, output) {
 
         df_data <- v$data$df_data
 
-        benchmarking_data_dl <- prepare_dl_data(
+        benchmarking_data_dl <- sdmbench::prepare_dl_data(
             input_data = df_data,
             partitioning_type = v$partitioning_type)
         progress_dl$inc(1/3, detail = "Training network...")
-        keras_results <- train_dl(benchmarking_data_dl)
-        keras_evaluation <- evaluate_dl(model_keras = keras_results$model,
+        keras_results <- sdmbench::train_dl(benchmarking_data_dl)
+        keras_evaluation <- sdmbench::evaluate_dl(model_keras = keras_results$model,
                                         input_data = benchmarking_data_dl)
 
         temp_fun <- function(model, input_data) {
@@ -235,9 +235,9 @@ server <- function(input, output) {
         }
 
         output$dl_auc <- renderText(paste("AUC: ",
-                                        get_dl_auc(keras_evaluation)))
+                                        sdmbench::get_dl_auc(keras_evaluation)))
         output$dl_map <- leaflet::renderLeaflet(
-            plot_dl_map(raster_data = v$data$raster_data,
+            sdmbench::plot_dl_map(raster_data = v$data$raster_data,
                         keras_model = keras_results$model,
                         custom_fun = temp_fun,
                         map_type = "interactive"))
